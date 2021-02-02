89b9f4847990eecc41160bb39cf4defee1215cde
Nick <mister.keyn@mail.ru>
2021-02-02 22:34:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 const getRandomNumber = function(min, max) {
   if (min >= max || min < 0 || max < 0)
     return false
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
 // from
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const getRandomNumberComma = function(min, max, comma) {
-    if (min >= max || min < 0 || max < 0)
-      return false
-      return (Math.random() * (max - min) + min).toFixed(comma);
-    }
-    // from
-    // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+const getRandomNumberComma = function(min, max, comma) {if (min >= max || min < 0 || max < 0)
+  return false
+return (Math.random() * (max - min) + min).toFixed(comma);
+}
+// from
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 
 
-
-
+export {getRandomNumber, getRandomNumberComma}
 
 
