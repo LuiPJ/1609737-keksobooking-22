bc23dc18b7e53cd2e4edf8b95fb0c7866f18de7b
Nick <mister.keyn@mail.ru>
2021-02-13 00:58:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const getRandomNumber = (min, max) => {
   if (min >= max || min < 0 || max < 0) {
     return -1
@@ -18,4 +20,47 @@ const getRandomNumberComma = (min, max, comma) => {
 
 export {getRandomNumber, getRandomNumberComma}
 
+const randomValues = (arr) => {
+  return arr.slice(getRandomNumber(1, arr.length - 1));
+}
 
+const TYPE = ['palace', 'flat', 'house', 'bungalow'];
+
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg.'];
+
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const CHEKOUT = ['12:00', '13:00', '14:00'];
+
+const CHECKIN = ['12:00','13:00','14:00'];
+
+const TITLE = ['Маленькая студия','Большая студия','Аппартаменты','Комната','Бунгало','Уютный домик','Домик у побережья'];
+
+const DESCRIPTION= ['Цена - качество','Красивый вид','Рядом магазины','Близко к остановке'];
+
+const MakeRandomItems = () => {
+  const LocationX = getRandomNumberComma(35.65000, 35.70000, 5);
+  const LocationY = getRandomNumberComma(139.70000, 139.80000, 5);
+
+  const addItems = {
+    author: {
+      avatar: `img/avatars/user0${getRandomNumber(1, 8)}.png`,
+    },
+    offer: {
+      title: randomValues(TITLE),
+      address: LocationX + ', ' + LocationY,
+      price: getRandomNumber(5000, 50000),
+      type: randomValues(TYPE),
+      rooms: getRandomNumber(1, 10),
+      guests: getRandomNumber(1, 5),
+      checkin: randomValues(CHECKIN),
+      checkout: randomValues(CHEKOUT),
+      features: randomValues(FEATURES),
+      description: randomValues(DESCRIPTION),
+      photos: randomValues(PHOTOS),
+    },
+  };
+  return addItems;
+}
+
+MakeRandomItems();
