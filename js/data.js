6361f5dc98f061151f4c7e57fd661e34fa28d04c
Nick <mister.keyn@mail.ru>
2021-02-26 20:22:58 +0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,34 @@
+import { getRandomNumber, getRandomNumberComma, getRandomValues } from './util.js'
+
+const createAd = (TITLE, TYPE, CHECKIN, CHEKOUT, FEATURES, DESCRIPTION, PHOTOS) => {
+  const locationX = getRandomNumberComma(35.65000, 35.70000, 5);
+  const locationY = getRandomNumberComma(139.70000, 139.80000, 5);
+
+  const addItems = {
+    author: {
+      avatar: `img/avatars/user0${getRandomNumber(1, 8)}.png`,
+    },
+    offer: {
+      title: getRandomValues(TITLE),
+      address: locationX + ', ' + locationY,
+      price: getRandomNumber(5000, 50000),
+      type: getRandomValues(TYPE),
+      rooms: getRandomNumber(1, 10),
+      guests: getRandomNumber(1, 5),
+      checkin: getRandomValues(CHECKIN),
+      checkout: getRandomValues(CHEKOUT),
+      features: getRandomValues(FEATURES),
+      description: getRandomValues(DESCRIPTION),
+      photos: getRandomValues(PHOTOS),
+    },
+    location: {
+      x: locationX,
+      y: locationY,
+    },
+  };
+  return addItems;
+}
+
+const ads = new Array(10).fill(null).map(() => createAd());
+
+export { ads, createAd };
