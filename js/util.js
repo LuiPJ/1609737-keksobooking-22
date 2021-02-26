66533faccfddff072af3721112000f7862288bef
Nick <mister.keyn@mail.ru>
2021-02-26 20:23:05 +0300

    Create util.js

---
+++ js/util.js
@@ -0,0 +1,19 @@
+const getRandomNumber = (min, max) => {
+  if (min >= max || min < 0 || max < 0) {
+    return -1
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+const getRandomNumberComma = (min, max, comma) => {
+  if (min >= max || min < 0 || max < 0) {
+    return -1
+  }
+  return Number((Math.random() * (max - min) + min).toFixed(comma));
+}
+
+const getRandomValues = (arr) => {
+  return arr.slice(getRandomNumber(1, arr.length - 1));
+}
+
+export {getRandomNumber, getRandomNumberComma, getRandomValues};
