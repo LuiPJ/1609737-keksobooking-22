088d33af0c217bc34c60b48a5ecef814e9df7f56
Nick <mister.keyn@mail.ru>
2021-03-05 17:01:14 +0300

    Create advt.js

---
+++ js/advt.js
@@ -0,0 +1,21 @@
+//const typeOfHousing = {'flat': 'Квартира', 'house': 'Дом', 'bungalow': 'Бунгало', 'palace': 'Дворец'};
+const createAdvert = (ad) => {
+  const adFacts = document.querySelector('#card').content.querySelector('.popup');
+
+  const adCase = adFacts.cloneNode(true);
+
+  adCase.querySelector('.popup__title').textContent = ad.offer.title;
+  adCase.querySelector('.popup__text--address').textContent = ad.offer.address;
+  adCase.querySelector('.popup__text--price').textContent = `${ad.offer.price}₽/ночь`;
+  adCase.querySelector('.popup__type').textContent = Object.values(ad.offer.type);
+  adCase.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  adCase.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin} выезд до ${ad.offer.checkout}`;
+  adCase.querySelector('.popup__features').textContent = ad.offer.features;
+  adCase.querySelector('.popup__description').textContent = ad.offer.description;
+  adCase.querySelector('.popup__photos').src = ad.offer.photos;
+  adCase.querySelector('.popup__avatar').src = ad.avthor.avatar;
+  adCase.querySelector('.popup__avatar').src = ad.author.avatar;
+
+  return adCase;
+}
+export { createAdvert };
