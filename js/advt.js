0691e24b549bfb8fc6c1ce09886d5e9ab1ce8dde
Nick <mister.keyn@mail.ru>
2021-03-07 21:52:45 +0300

    Update advt.js

--- js/advt.js
+++ js/advt.js
@@ -7,15 +7,18 @@ const createAdvert = (ad) => {
   adCase.querySelector('.popup__title').textContent = ad.offer.title;
   adCase.querySelector('.popup__text--address').textContent = ad.offer.address;
   adCase.querySelector('.popup__text--price').textContent = `${ad.offer.price}₽/ночь`;
-  adCase.querySelector('.popup__type').textContent = Object.values(ad.offer.type);
+  adCase.querySelector('.popup__type').textContent = (ad.offer.type);
   adCase.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
   adCase.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin} выезд до ${ad.offer.checkout}`;
   adCase.querySelector('.popup__features').textContent = ad.offer.features;
   adCase.querySelector('.popup__description').textContent = ad.offer.description;
   adCase.querySelector('.popup__photos').src = ad.offer.photos;
-  adCase.querySelector('.popup__avatar').src = ad.avthor.avatar;
+  adCase.querySelector('.popup__avatar').src = ad.author.avatar;
   adCase.querySelector('.popup__avatar').src = ad.author.avatar;
 
   return adCase;
 }
+
+
 export { createAdvert };
+
